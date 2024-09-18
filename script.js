ef30aaae3f40e09fabf196dda2f76732cb6c32d8
Ruman 03 <rumanasif3@gmail.com>
2024-09-18 14:46:02 +0500

    Update GAME

--- script.js
+++ script.js
@@ -219,15 +219,3 @@ function musicOff(){
     document.querySelector('.cross').classList.toggle('crossActive');
 }
 
-
-// Game Height Issue
-function setContainerHeight() {
-    const vh = window.innerHeight * 0.01; // Get the correct vh unit
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
-
-// Call the function on page load
-setContainerHeight();
-
-// Update the height on window resize
-window.addEventListener('resize', setContainerHeight);
